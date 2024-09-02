bab3aab3a4eefb096a83a6fee8eea73fe4a623d5
slobo <suslobo@hotmail.es>
2024-09-02 17:04:18 +0200

    La importancia de los cliclos de vida, para qué se utiliza cada uno y los más utilizados

--- src/app/products/pages/product/product.component.ts
+++ src/app/products/pages/product/product.component.ts
@@ -13,16 +13,18 @@ export class ProductComponent implements OnInit, OnChanges, DoCheck, AfterConten
   constructor() {
     console.log('Constructor');
   }
-
+  //para hacer la primera petición HTTP
   ngOnInit(): void {
     console.log('ngOnInit');
   }
-
+  //se utiliza si vas a estar pendiente de los cambios, de las propiedades... o de los inputs que tenemos en price.component.ts (el hijo)
   ngOnChanges(changes: SimpleChanges): void {
     console.log({changes})
     console.log('ngOnChanges');
   }
-
+  //ngDoCheck,ngAfterContentInit,ngAfterContentChecked, ngAfterViewInit
+  //estos son cuando suceden cambios ya sea en inputs o en propiedades cuando el componente detecta algún cambio
+  //se usan muy poco
   ngDoCheck(): void {
     console.log('ngDoCheck');
   }
@@ -41,7 +43,7 @@ export class ProductComponent implements OnInit, OnChanges, DoCheck, AfterConten
   ngAfterViewChecked(): void {
     console.log('ngAfterViewChecked');
   }
-
+  //hace la limpieza si en el ngOnInit tenemos algún tipo de lista que se está ejecuntado a lo largo que el componente esté en pantalla
   ngOnDestroy(): void {
     console.log('ngOnDestroy');
   }
